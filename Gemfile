4a1f87a0698e52173261a9797ba0501d94dd746a
Nicolas LACOURTE <nicolas@dotinfra.fr>
2013-01-16 00:25:26 +0100

    Update HTTP Method

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source :rubygems
 
-gem 'sinatra', '1.1.0'
-gem 'thin', '1.2.7'
+gem 'sinatra'
+gem 'thin'
 gem 'json'
--- app/main.rb
+++ app/main.rb
@@ -4,13 +4,14 @@ require 'benchmark'
 require 'json'
 require 'net/http'
 require 'net/https'
+require 'uri'
 
 class NetChecks < Sinatra::Base
 
 # If try to access to root, do redirect => skillstar.com
 # HTTP Response : 302
   get '/' do
-    redirect 'http://www.skillstar.com'
+    redirect 'http://www.dotinfra.fr'
   end
 
 # Getting JSON serialized return of checks
@@ -18,24 +19,31 @@ class NetChecks < Sinatra::Base
     content_type :json
 
     url = "#{params[:url]}"
-    path = "#{params[:path]}"
+    proxy = "#{params[:proxy]}"
 
-    stats = Benchmark.realtime { @result = http_code("#{url}", "#{path}") }
+    stats = Benchmark.realtime { @result = http_get(URI(proxy), URI(url)) }
 
       json = { :HTTP_Check => {
-        :request_to              => "#{url}#{path}",
-        :response_code           => "#{@result}",
-        :response_time           => "#{stats}"
-      }}
+        :request_to              => "#{url}",
+        :response_code           => "#{@result.code}",
+        :response_time           => "#{stats}",
+        :cachecontrol            => "#{@result.header.get_fields('Cache-Control').first.to_s}"
+      }
+      }
       JSON.pretty_generate(json)
   end
 
 # Define methods
-  def http_code(website, path)
-    user_agent = 'Mozilla/5.0 (Heroku; availability check; get code)'
-    @connect = Net::HTTP.new("#{website}", "80")
-    resp, data = @connect.get("#{path}", {'User-Agent' => user_agent})
+  def http_code(proxy, url)
+    resp = http_get(URI(proxy), URI(url))
     resp.code
+  end
+  def http_get(proxy,uri)
+    user_agent = 'Mozilla/5.0 (dotINFRA; remote check)'
+    connect = Net::HTTP::Proxy(proxy.host,proxy.port).new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => user_agent})
+    req.basic_auth uri.user, uri.password
+    resp, data = connect.request(req)
   end
 
 end
